bfb7ac207a2590a1cd3ddccea25479bd47526cc2
git@github.com <dp0613@yandex.com>
2017-10-29 09:56:48 +0700

    edit port

--- app.js
+++ app.js
@@ -29,6 +29,4 @@ app.use(function(req, res, next){
 });
 
 // Listen port 3000
-app.listen(3000, function(){
-	console.log('Server started on port 3000');
-});
+app.listen(process.env.PORT || 3000);
