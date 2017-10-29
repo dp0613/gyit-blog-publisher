ecc0edd3803ea9678e4fff159e5fa382bcdc72cf
git@github.com <dp0613@yandex.com>
2017-10-29 09:43:57 +0700

    add postgre heroku uri

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const {Client} = require('pg');
-const connect = 'postgre://dp0613:dp0613ishandsomeguy@localhost/gyit-post-publisher';
+const connectLocal = 'postgre://dp0613:dp0613ishandsomeguy@localhost/gyit-post-publisher';
+const connectHeroku = 'postgres://jcgmrripmntmcb:19c789bf00003b4e648dba946897fbf3dfcba0ae4d1e528f7d759eab90af7cb8@ec2-174-129-227-116.compute-1.amazonaws.com:5432/d2fgou03np12to';
 
 //Connect database
 const client = new Client({
-  connectionString: connect,
+  connectionString: connectHeroku,
 });
 client.connect();
 
